19cb65a3085a654f4986d977d9eab0772ec9c49b
Raman_kp <38486598+ramkp990@users.noreply.github.com>
2020-06-06 14:16:25 +0530

    Update collecting_depth_data.py

--- collecting_depth_data.py
+++ collecting_depth_data.py
@@ -10,20 +10,20 @@ config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 profile = pipeline.start(config)
 depths=[]
-print("going to start")
+print("recognizing hand gesture")
 time.sleep(1)
 tend=time.time()+1*2
-i=0
+#collecting depth data
 try:
 	while time.time() <tend:
 		frames=pipeline.wait_for_frames()
-		color_im=np.asarray(frames.get_depth_frame().get_data())
-		depths.append(color_im)
+		depth_im=np.asarray(frames.get_depth_frame().get_data())
+		depths.append(depth_im)
 		time.sleep(0.00001)
+		#considering first 25 frames only
 		if len(depths)==25:
 			break
 finally:
-	print(i)
 	print("hand gesture  recognized")
 	leng=len(depths)
 	w,h=depths[0].shape
@@ -36,6 +36,7 @@ finally:
 		c_new=depths[i]
 		c=np.concatenate((c,c_new),axis=0)
 	c=c.T
+	#writing depth data to a csv file
 	if len(depths)==25:
 		print("writing to file")
 		with open('test.csv','a',newline='')as file:
